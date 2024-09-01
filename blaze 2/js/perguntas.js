12524aadbd131950016550e4e8f4e99e502c6f9c
VendedorDeGalos <127774370+VendedorDeGalos@users.noreply.github.com>
2024-09-01 16:17:22 -0300

    Add files via upload

---
+++ blaze 2/js/perguntas.js
@@ -0,0 +1,101 @@
+export const perguntas = [
+    {
+        enunciado: "com esse mal presentimento, você se leventa da espreguiçadeira e vai espiar pela janela e ve a polícia federal tentando arrombar sua porta, oque você fara?",
+        alternativas: [
+            {
+                texto: "tentar se esconder",
+                afirmacao: [
+                    "Após ver a polícia batendo na porta da sua mansão você tentou se esconder",
+                    "quando você viu a polícia batendo na sua porta, você tentou se esconder"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "fugir",
+                afirmacao: [
+                    "Após ver a polícia batendo na porta da sua mansão você tentou fugir",
+                    "quando você viu a polícia batendo na sua porta, você tentou fugir"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "desesperado, você tenta pensar em algum lugar para se esconder.",
+        alternativas: [
+            {
+                texto: "se esconder no porão",
+                afirmacao: [
+                    "no porão, você entra dentro de uma caixa de papelão que tu achou, mas não leva muito tempo até os cães te farejarem, você é preso e condenado a prisão perpértua",
+                    "no porão, você se espreme atrás de um gabinete velho na esperança de não te acharem, mas não leva muito tempo até os cães te farejarem, você é preso e condenado a prisão perpértua"
+                ],
+            },
+            {
+                texto: "se esconder debaixo da cama",
+                afirmacao: [
+                    "debaixo da cama, você corre pro seu quarto e se espreme debaixo da sua cama, mas não leva muito tempo até os cães te farejarem, você é preso e condenado a prisão perpértua"
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "desesperado você pensa em uma rota de fuga",
+        alternativas: [
+            {
+                texto: "usar a passagem secreta",
+                afirmacao: [
+                    "por uma passagem secreta que foi contruida na mansão, você corre até a biblioteca e tenta se lembrar de qual livro tem que puxar para abrir a passagem, ",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "correr até o heliponto",
+                afirmacao: [
+                    "de helicoptero, você sobe as escadas até chegar no heliponto na cobertura, você sobe no helicoptero e descobre que você não sabe pilotar helicopteros",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "você corre até a biblioteca e tenta se lembrar de qual livro tem que puxar para abrir a passagem",
+        alternativas: [
+            {
+                texto: "puxar aleatoriamente",
+                afirmacao: [
+                    "você começa a puxar livros aleatorios até que uma das estantes abre, revelando um elevador pro subsolo,",
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "tentar outra rota de fuga",
+                afirmacao: [
+                    "você não consegue se lembrar de qual livro tem que puxar, então você tenta correr até o heliponto na cobertura, enquanto sobe as escadas, você torçe o tornozelo e cai de cabeça no corrimão",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "uma das estantes abre, revelando um elevador, você desce o elevador até chegar o data center da blaze, você passa pelas fileiras de computadores que hosteiam a blaze.com",
+        alternativas: [
+            {
+                texto: "seguir em frente",
+                afirmacao: [
+                    "você desce o elevador até chegar no data center da blaze, passando pelas fileiras de computadores até chegar em um tunel,"
+                ],
+                proxima: 5,
+            },
+    
+        ]
+    },
+    {
+        enunciado: "apos inumeras fileiras de servidores, você chega em um tunel e depois de horas atravessando o tunel, você sai em terreno baldio no meio da cidade",
+        alternativas: [
+            {
+                texto: "você escapou com sucesso",
+                afirmacao: [
+                    "que sai no meio da cidade"
+                ],
+            }
+        ]
+    },
+];
